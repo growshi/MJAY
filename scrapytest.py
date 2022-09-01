4cbb9499170a8de0ddef7fda7c0d4a6c1472ade2
growshi <mirulza12@gmail.com>
2022-09-01 14:51:34 +0800

    testing

--- scrapytest.py
+++ scrapytest.py
@@ -2,7 +2,15 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ['http://brickset.com/sets/year-2019']
+    start_urls = ['http://172.18.58.80/varsity/']
+
+    def parse(self, response):
+     css_selector = 'img'
+     for x in response.css(css_selector):
+      newsel = '@src'
+      yield {
+       'Image Link': x.xpath(newsel).extract_first(),
+}
 process = CrawlerProcess()
 process.crawl(NewSpider)
 process.start()
